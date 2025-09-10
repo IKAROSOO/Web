29feda83acd7c7a4587c0f114e13ceef0bde392d
IKAROS <1027alex@gmail.com>
2025-09-10 21:17:42 +0900

    Make Directory

--- ImageToText/GeminiApi Test/api.py
+++ ImageToText/GeminiApi Test/api.py
@@ -1,8 +1,12 @@
 import google.generativeai as genai
 import json
+import os
 
 # 해당 config.json의 파일경로는 컴퓨터에 따라 변해야한다.
-CONFIG_PATH = 'Personal/Image/config.json'
+# os 라이브러리를 활용해 항상 config.json의 절대경로를 파악
+file_path = os.path.dirname(os.path.abspath(__file__))
+script_path = os.path.dirname(file_path)
+CONFIG_PATH = os.path.join(script_path, 'config.json')
 
 # json파일을 읽어와서 API_KEY값을 뽑아내는 구문
 with open(CONFIG_PATH, 'r') as f:
